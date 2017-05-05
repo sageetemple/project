58ef63f51fe4f89117cfca755ed7692b73377767
mandylee900125 <LeeM3209@sduhsd.lan>
2017-05-05 10:24:49 -0700

    chnages

--- Final Project/javaScript.js
+++ Final Project/javaScript.js
@@ -1,10 +1,25 @@
+var angle = PI / 4;
+
+var slider;
+
 function setup(){
     createCanvas(400,400);
+    slider = createSlider(0,TW0_PI, PI/4);
 }
 function draw() {
     background(51);
-
-    var len = 100;
+    angle = slider.value();
     stroke(225);
-    line(200, height, 200, height - len);
+    translate(200,height);
+    branch(100);
+}
+function branch(len){
+
+    line(0, 0, 0, - len);
+    translate(0,-len);
+    rotate(angle);
+    if(len < 4) {
+        branch(len * 0.67);
+    }
+    line(0, 0, 0, -len * 0.67);
 }
